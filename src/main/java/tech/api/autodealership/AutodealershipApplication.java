7422dd38b8b6ba68226da9b67080b2e92605410e
Caio Antonio <caio.antonio.c@outlook.com>
2024-01-06 19:42:30 -0300

    project init

---
+++ src/main/java/tech/api/autodealership/AutodealershipApplication.java
@@ -0,0 +1,13 @@
+package tech.api.autodealership;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutodealershipApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutodealershipApplication.class, args);
+	}
+
+}
---
+++ src/test/java/tech/api/autodealership/AutodealershipApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.api.autodealership;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutodealershipApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
