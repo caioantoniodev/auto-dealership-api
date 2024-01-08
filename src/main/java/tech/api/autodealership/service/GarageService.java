b8aedabaf439725d37e740af76c817c3f74490ae
Caio Antonio <caio.antonio.c@outlook.com>
2024-01-08 14:23:25 -0300

    implements retrieve garage with respective vehicles

--- src/main/java/tech/api/autodealership/service/GarageService.java
+++ src/main/java/tech/api/autodealership/service/GarageService.java
@@ -5,14 +5,24 @@ import org.springframework.stereotype.Service;
 import software.amazon.awssdk.enhanced.dynamodb.DynamoDbTable;
 import tech.api.autodealership.entity.Garage;
 
+import java.util.List;
+
 @RequiredArgsConstructor
 @Service
-public class DatabaseService {
+public class GarageService {
 
     private final DynamoDbTable<Garage> garageTable;
 
     public void save(Garage garage) {
         var creationGarage = garage.forCreation();
         garageTable.putItem(creationGarage);
+    }
+
+    public List<Garage> findAll() {
+        return garageTable.scan()
+                .items()
+                .stream()
+                .toList();
+
     }
 }
