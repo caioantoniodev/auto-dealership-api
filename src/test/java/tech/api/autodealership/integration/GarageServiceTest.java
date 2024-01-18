f51a92ebd0dbbd49cd652ce9a790b391f483d643
Caio Antonio <caio.antonio.c@outlook.com>
2024-01-17 22:02:16 -0300

    added test for create garage

--- src/test/java/tech/api/autodealership/integration/GarageServiceTest.java
+++ src/test/java/tech/api/autodealership/integration/GarageServiceTest.java
@@ -32,4 +32,14 @@ public class GarageServiceTest extends AbstractDynamoDbContainer {
         Assertions.assertTrue(result.isPresent());
         Assertions.assertNotNull(result.get());
     }
+
+    @Test
+    void shouldBeCreate() {
+        var garage = GarageMock.any();
+        this.garageService.save(garage);
+
+        var result = this.garageService.findAll();
+        Assertions.assertNotNull(result);
+        Assertions.assertEquals(1, result.size());
+    }
 }
